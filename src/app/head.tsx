86a3b6c7f564e03a7ada06448920ca86482c672d
Byron Wade <bcw1995@gmail.com>
2023-02-07 09:28:23 -0800

    added alot of the core stuff

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+	return (
+		<>
+			<title>Reddit</title>
+			<meta content='width=device-width, initial-scale=1' name='viewport' />
+			<meta name='description' content='Reddit Clone' />
+			<link rel='icon' href='/favicon.ico' />
+		</>
+	);
+}
